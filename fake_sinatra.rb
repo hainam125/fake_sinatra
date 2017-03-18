4608e8be521ccd66b531aee271e8958dfdcad089
NamNH <hainam125@gmail.com>
2017-03-18 21:38:11 +0700

    other method

--- fake_sinatra.rb
+++ fake_sinatra.rb
@@ -1,11 +1,11 @@
-def get(path, &block)
+def general_request_method(request_method, path, &block)
 	middleware = Class.new
 	middleware.send(:define_method, 'initialize') do |app|
 		@app = app
 	end
 
 	middleware.send(:define_method, "call") do |env|
-		if env['REQUEST_METHOD'] == 'GET'&& env['REQUEST_PATH'] == self.class.path
+		if env['REQUEST_METHOD'] == self.class.request_method && env['REQUEST_PATH'] == self.class.path
 			action
 		else
 			@app.call env
@@ -20,11 +20,36 @@ def get(path, &block)
 		def path
 			@path
 		end
+
+		def request_method=(rm)
+			@rm = rm
+		end
+
+		def request_method
+			@rm
+		end
 	end
 
 	middleware.send(:define_method, "action", &block)
 
 	middleware.path = path
+	middleware.request_method = request_method
 
 	use middleware
+end
+
+def get(path, &block)
+	general_request_method("GET", path, &block)
+end
+
+def post(path, &block)
+	general_request_method("POST", path, &block)
+end
+
+def put(path, &block)
+	general_request_method("PUT", path, &block)
+end
+
+def delete(path, &block)
+	general_request_method("DELETE", path, &block)
 end
